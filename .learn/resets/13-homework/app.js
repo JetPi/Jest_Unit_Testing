cf7272e72b9f47179db0d53881172f699f664c41
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-07-15 13:42:08 -0400

    Done

---
+++ .learn/resets/13-homework/app.js
@@ -0,0 +1,13 @@
+function fromEuroToDollar (amountInEuro){
+    return amountInEuro*1.2
+}
+
+function fromDollarToYen (amountInDollars){
+    return num1+num2
+}
+
+function fromYenToPound (amountInYen){
+    return num1+num2
+}
+
+module.exports = {fromEuroToDollar, fromDollarToYen, fromYenToPound}
